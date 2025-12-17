7293774df4881fb67aa928d7131448adf8d4aa82
Bruno Quintela <bruno.quintela@akilis.ai>
2025-12-17 16:15:10 +0000

    fix

--- components/Gallery/components/KeyboardShortcuts.tsx
+++ components/Gallery/components/KeyboardShortcuts.tsx
@@ -6,13 +6,13 @@ export function KeyboardShortcuts() {
       className="corner-text corner-text-shortcuts"
       aria-label="Keyboard shortcuts"
     >
-      <p>H - TOGGLE SETTINGS</p>
-      <p>1 - DATAMOSH</p>
-      <p>2 - PIXEL SORT</p>
-      <p>3 - DIGITAL STATIC</p>
-      <p>4 - STATIC SWEEP</p>
-      <p>5 - GLITCH WIPE</p>
-      <p>6 - ANALOG DECAY</p>
+      <div>TOGGLE SETTINGS - H</div>
+      <div>DATAMOSH - 1</div>
+      <div>PIXEL SORT - 2</div>
+      <div>DIGITAL STATIC - 3</div>
+      <div>STATIC SWEEP - 4</div>
+      <div>GLITCH WIPE - 5</div>
+      <div>ANALOG DECAY - 6</div>
     </nav>
   );
 }
